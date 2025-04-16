00b4b43a34831687fa0a270ba10da4f7a8073daa
Antonio <webmaster@mallorkagps.com>
2025-04-16 18:31:40 -0400

    Update NucleiFuzzer.sh

--- NucleiFuzzer.sh
+++ NucleiFuzzer.sh
@@ -183,8 +183,32 @@ validate_urls() {
 run_nuclei() {
     local url_file="$1"
     echo -e "${GREEN}Running Nuclei on URLs from $url_file...${RESET}"
-    httpx -silent -mc 200,204,301,302,401,403,405,500,502,503,504 -l "$url_file" \
-        | nuclei -t "$TEMPLATE_DIR" -dast -severity low,medium,high,critical -tags cve,security-misconfiguration,fuzz,xss,sqli,rce,exposure,token,misconfig,ssrf,idor,auth -etags dos,brute -rate-limit "$RATE_LIMIT" -c 20 -retries 3 -j -irr -nc -headless -no-stdin -bs 100 -timeout 15 -o "$OUTPUT_FOLDER/nuclei_results.txt" -es info -stats -si 60
+
+    httpx -silent -mc 200,204,301,302,401,403,405,500,502,503,504 \
+  -l "$url_file" \
+  -o "$OUTPUT_FOLDER/httpx_live.txt"
+
+    nuclei -l "$OUTPUT_FOLDER/httpx_live.txt" -im list \
+      -dast \
+      -t /home/lynx/.local/nuclei-templates/ \
+      -severity low,medium,high,critical \
+      -tags cve,security-misconfiguration,fuzz,xss,sqli,rce,exposure,token,misconfig,ssrf,idor,auth \
+      -etags dos,brute \
+      -rate-limit "$RATE_LIMIT" \
+      -c 20 \
+      -retries 3 \
+      -j \
+      -irr \
+      -nc \
+      -v \
+      -headless \
+      -no-stdin \
+      -bs 100 \
+      -timeout 15 \
+      -o "$OUTPUT_FOLDER/nuclei_results.txt" \
+      -es info \
+      -stats \
+      -si 60
 }
 
 # Main logic
