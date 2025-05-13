2e6918d9e79ecb3aa5f60122682e9c2f678393b1
Antonio <webmaster@mallorkagps.com>
2025-05-13 13:32:22 -0400

    Update NucleiFuzzer.sh

--- NucleiFuzzer.sh
+++ NucleiFuzzer.sh
@@ -61,23 +61,6 @@ log() {
     fi
 }
 
-# Check prerequisites
-check_prerequisite() {
-    local tool="$1"
-    local install_command="$2"
-    if ! command -v "$tool" &> /dev/null; then
-        log "INFO" "Installing $tool..."
-        if ! eval "$install_command"; then
-            log "ERROR" "Failed to install $tool. Exiting."
-            exit 1
-        fi
-        # Ensure uro is in PATH
-        if [ "$tool" = "uro" ] && [ -f "$HOME/.local/bin/uro" ]; then
-            log "INFO" "Adding $HOME/.local/bin to PATH for uro."
-            export PATH="$HOME/.local/bin:$PATH"
-        fi
-    fi
-}
 
 # Clone repositories
 clone_repo() {
@@ -119,13 +102,6 @@ mkdir -p "$OUTPUT_FOLDER"
 TEMPLATE_DIR=${TEMPLATE_DIR:-"$HOME_DIR/nuclei-templates"}
 
 # Install dependencies
-check_prerequisite "nuclei" "go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest"
-check_prerequisite "httpx" "go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest"
-check_prerequisite "uro" "pip3 install uro"
-check_prerequisite "katana" "go install -v github.com/projectdiscovery/katana/cmd/katana@latest"
-check_prerequisite "waybackurls" "go install github.com/tomnomnom/waybackurls@latest"
-check_prerequisite "gauplus" "go install github.com/bp0lr/gauplus@latest"
-check_prerequisite "hakrawler" "go install github.com/hakluke/hakrawler@latest"
 clone_repo "https://github.com/0xKayala/ParamSpider" "$HOME_DIR/ParamSpider"
 clone_repo "https://github.com/projectdiscovery/nuclei-templates.git" "$HOME_DIR/nuclei-templates"
 
